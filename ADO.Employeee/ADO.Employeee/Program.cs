14514b107c80e15e5ebfbb71a49ec6370c0c3b69
sayalikeche <kechesayali11@gmail.com>
2021-12-22 18:16:59 +0530

    [sayali Added ADO.Employee]

---
+++ ADO.Employeee/ADO.Employeee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.Employeee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO.EmployeePayroll");
+        }
+    }
+}
